c747f2e793837f4f294e32e160c16d241fddf971
benshaw2 <bendougshaw@gmail.com>
2026-03-20 15:54:42 -0600

    update imports

--- src/symdisc/enforcement/regularization/__init__.py
+++ src/symdisc/enforcement/regularization/__init__.py
@@ -1,6 +1,7 @@
 from .penalties import invariance_penalty, equivariance_penalty
 from .diagonal import diagonalize, diagonalize_channels, sum_fields
 from . import schedules
+from .utilities import _maybe_call_field, as_field_lastdim, make_pairer
 
 __all__ = [
     "invariance_penalty",
@@ -9,4 +10,7 @@ __all__ = [
     "diagonalize_channels",
     "sum_fields",
     "schedules",
+    "_maybe_call_field",
+    "as_field_lastdim",
+    "make_pairer"
 ]
