18d2f6892e164850280f77c41417dfc24f022866
benshaw2 <bendougshaw@gmail.com>
2026-03-28 16:10:12 -0600

    Fix imports, dependencies

--- src/symdisc/enforcement/regularization/__init__.py
+++ src/symdisc/enforcement/regularization/__init__.py
@@ -1,14 +1,33 @@
-from .penalties import invariance_penalty, equivariance_penalty
-from .diagonal import diagonalize, diagonalize_channels, sum_fields
+from .penalties import \
+    (invariance_penalty,
+     equivariance_penalty,
+     forward_with_invariance_penalty,
+     forward_with_equivariance_penalty
+     )
+from .diagonal import \
+    (diagonalize,
+     diagonalize_channels,
+     sum_fields,
+     pack_flat,
+     unpack_flat,
+     build_flat_mask,
+     lift_field_to_flat_segment
+     )
 from . import schedules
 from .utilities import _maybe_call_field, as_field_lastdim, make_pairer
 
 __all__ = [
     "invariance_penalty",
     "equivariance_penalty",
+    "forward_with_invariance_penalty",
+    "forward_with_equivariance_penalty",
     "diagonalize",
     "diagonalize_channels",
     "sum_fields",
+    "pack_flat",
+    "unpack_flat",
+    "build_flat_mask",
+    "lift_field_to_flat_segment",
     "schedules",
     "_maybe_call_field",
     "as_field_lastdim",
